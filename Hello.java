20249b75f17c18bd0abfbb13925c1715db9ad097
Riyadh <riyadhbrurcse@gmail.com>
2020-03-21 00:35:00 +0600

    Change one method

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static vaid main(String[] args){
         System.out.println("Vs code in github upload");
-        greeting("Riyadh");
+        greeting("Github");
     }
     public static void greeting(String name){
         System.out.println("Welcome" + name);
