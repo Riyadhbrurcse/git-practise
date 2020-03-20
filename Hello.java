4d17fbab2f1efcde94c4c2546194070a46b9c36d
Riyadh <riyadhbrurcse@gmail.com>
2020-03-21 00:19:29 +0600

    Added a new method called display to Hello.java file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static vaid main(String[] args){
         System.out.println("Vs code in github upload");
+        greeting("Riyadh");
+    }
+    public static void greeting(String name){
+        System.out.println("Welcome" + name);
     }
 }
