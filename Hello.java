b50231a63a4a78b70f2e3186dafc009ee6ae29de
Riyadh <riyadhbrurcse@gmail.com>
2020-03-21 13:35:34 +0600

    new branch which is called anotherbranch

--- Hello.java
+++ Hello.java
@@ -1,23 +1,14 @@
-//masterBranch
+//anotherbranch
 public class Hello{
 
     public static vaid main(String[] args){
 
         System.out.println("Vs code in github upload");
-        
-        display();
+    
        
     }
-    public static vaid greeting(String name){
-
-        System.out.println("welcome" + name);
+    public static void message(){
+        System.out.println("Now we are able to contribute an open source");
     }
-    public static void display(String name){
 
-        System.out.println("Welcome" + name);
-    }
-    public static int sqr(int x){
-
-        return x*x;
-    }
 }
