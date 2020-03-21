1363e466b2f8efbb966a023ad709a9bf308cf896
Riyadh <riyadhbrurcse@gmail.com>
2020-03-21 13:29:26 +0600

    master branch is updated

--- Hello.java
+++ Hello.java
@@ -1,14 +1,23 @@
 //masterBranch
 public class Hello{
+
     public static vaid main(String[] args){
+
         System.out.println("Vs code in github upload");
         
         display();
+       
+    }
+    public static vaid greeting(String name){
+
+        System.out.println("welcome" + name);
     }
     public static void display(String name){
+
         System.out.println("Welcome" + name);
     }
     public static int sqr(int x){
+
         return x*x;
     }
 }
